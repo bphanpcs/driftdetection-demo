5aa8f1ce15080898552ca6ed4fcf2c01277af5f7
Binh Phan <bphan@paloaltonetworks.com>
2023-04-20 19:12:51 -0700

    added yor tags

--- ec2-swaggerapi.tf
+++ ec2-swaggerapi.tf
@@ -4,19 +4,19 @@ provider "aws" {
 
 resource "aws_security_group" "example" {
   name_prefix = "example"
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["68.251.60.220/32"]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["68.251.60.220/32"]
   }
 
   egress {
@@ -25,27 +25,33 @@ resource "aws_security_group" "example" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-data "aws_subnet" "example" {
-  vpc_id = data.aws_vpc.default.id
   tags = {
-    "Name" = "Public Subnet"
+    git_commit           = "6d7c2c268906fca73e0a951e37bc0de065b300cc"
+    git_file             = "ec2-swaggerapi.tf"
+    git_last_modified_at = "2023-04-21 01:54:33"
+    git_last_modified_by = "bphan@paloaltonetworks.com"
+    git_modifiers        = "bphan"
+    git_org              = "bphanpcs"
+    git_repo             = "driftdetection-demo"
+    yor_trace            = "518ec5a6-c623-4572-9b26-bec1a1ed5a75"
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-081a3b9eded47f0f3"
-  instance_type = "t2.micro"
-  key_name      = "bphan-aws-key123"
-  subnet_id     = data.aws_subnet.example.id
+  ami                    = "ami-081a3b9eded47f0f3"
+  instance_type          = "t2.micro"
+  key_name               = "bphan-aws-key123"
   vpc_security_group_ids = [aws_security_group.example.id]
   tags = {
-    Name = "example-instance"
+    Name                 = "example-instance"
+    git_commit           = "6d7c2c268906fca73e0a951e37bc0de065b300cc"
+    git_file             = "ec2-swaggerapi.tf"
+    git_last_modified_at = "2023-04-21 01:54:33"
+    git_last_modified_by = "bphan@paloaltonetworks.com"
+    git_modifiers        = "bphan"
+    git_org              = "bphanpcs"
+    git_repo             = "driftdetection-demo"
+    yor_trace            = "55af62c8-d9c5-4731-be46-a398012970c3"
   }
 
   user_data = <<-EOF
